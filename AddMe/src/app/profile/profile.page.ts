ab93d5bdd744ef0588edc17be5f551be78c55eb8
Jj139872 <joannajohnsc@gmail.com>
2019-11-20 05:28:40 -0500

    <added buttons and page links>

--- AddMe/src/app/profile/profile.page.ts
+++ AddMe/src/app/profile/profile.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseBackendService } from '../firebase-backend.service';
+import { CameraPage } from '../camera/camera.page';
+import { Router } from '@angular/router'
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
@@ -9,12 +12,24 @@ export class ProfilePage implements OnInit {
   private profile: {};
   private uid: string;
   private firebase: FirebaseBackendService;
-  constructor() {
+  constructor(private router: Router) {
     // TODO update so the page gets logged in user's uid
     this.uid = "";
     this.firebase = new FirebaseBackendService(this.uid);
     this.profile = this.firebase.getUserData();
     console.log(this.profile);
+  }
+
+  goToCamera() {
+    this.router.navigate(['camera']);
+  }
+  
+  goToContacts() {
+    this.router.navigate(['contacts']);
+  }
+
+  goToHome() {
+    this.router.navigate(['home']);
   }
 
   ngOnInit() {
