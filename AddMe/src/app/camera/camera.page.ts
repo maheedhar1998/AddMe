96e4dd5591f075f6087ac99ccced42ed270ac034
maheedhar1998 <maheedhar1998@hotmail.com>
2019-12-01 23:53:26 -0500

    Update experimented with qr code scanner functionality

--- AddMe/src/app/camera/camera.page.ts
+++ AddMe/src/app/camera/camera.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 
 @Component({
   selector: 'app-camera',
@@ -8,13 +9,24 @@ import { Router } from '@angular/router';
 })
 export class CameraPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private qrScanner: QRScanner) { }
 
   goToHome() {
     this.router.navigate(['home']);
   }
 
   ngOnInit() {
+    this.qrScanner.prepare().then((state: QRScannerStatus) => {
+      if(state.authorized) {
+        let scan = this.qrScanner.scan().subscribe((txt: string) => {
+          console.log("scanned: ", txt);
+          this.qrScanner.hide();
+          scan.unsubscribe();
+        });
+      } else if(state.denied) {
+        this.qrScanner.openSettings();
+      }
+    }).catch((e: any) => console.log('Error: ', e));
   }
 
 }
