1c16e1da9580f420dba48ba9ddc75b07bb8f6fea
samuelmenkus <smenkus1@gmail.com>
2020-02-19 19:24:13 -0500

    crud update for social accounts

--- src/app/contacts/contacts.page.ts
+++ src/app/contacts/contacts.page.ts
@@ -13,8 +13,15 @@ import { NavParams } from '@ionic/angular';
 export class ContactsPage implements OnInit {
   private firebase: FirebaseBackendService;
   private type: string;
+  private id: string;
+  private username: string;
+  private url: string;
   private socialAccounts: backend.socialAccount[];
+  private adding: boolean = false;
   constructor(private router: Router, private navParam: NavParams) {
+    this.id = "";
+    this.username = "";
+    this.url = "";
     firebase.auth().onAuthStateChanged(firebaseUser => {
       if(!firebaseUser)
       {
@@ -36,8 +43,18 @@ export class ContactsPage implements OnInit {
     this.router.navigate(['home']);
   }
 
+  add() {
+    this.adding = true;
+  }
+
+  addSMAccount() {
+    this.firebase.addSocialAccount(this.type, new backend.socialAccount(this.id,this.username,this.url));
+    this.adding = false;
+  }
+
   ngOnInit() {
   }
+
   window() {
     window.open(this.socialAccounts[0].getUrl);
   }
