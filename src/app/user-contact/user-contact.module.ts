491ea3e514bcba447c414ca1371562100bceec18
maheedhar1998 <maheedhar1998@hotmail.com>
2020-02-29 04:02:49 -0500

    Update added usercontact page adjusted profile pageui and updated its backend

---
+++ src/app/user-contact/user-contact.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { UserContactPage } from './user-contact.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UserContactPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [UserContactPage]
+})
+export class UserContactPageModule {}
