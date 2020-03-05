50406d9b8d872282204f11740b65fcf47b50b1b3
samuelmenkus <smenkus1@gmail.com>
2020-03-04 19:48:22 -0500

    editing social media accounts

--- src/app/contacts/contacts.page.ts
+++ src/app/contacts/contacts.page.ts
@@ -17,13 +17,17 @@ export class ContactsPage implements OnInit {
   private username: string;
   private url: string;
   private socialAccounts: backend.socialAccount[];
+  private editing: boolean;
   private adding: boolean;
+  private mode: boolean;
   private none: boolean;
   constructor(private router: Router, private navParam: NavParams) {
     this.id = "";
     this.username = "";
     this.url = "";
     this.adding = false;
+    this.editing = false;
+    this.mode = false;
     this.none = false;
     firebase.auth().onAuthStateChanged(firebaseUser => {
       if(!firebaseUser)
@@ -57,14 +61,33 @@ export class ContactsPage implements OnInit {
     this.adding = true;
   }
 
+  edit() {
+    this.editing = true;
+  }
+
+  modeChange() {
+    if(this.mode == false) {
+      this.mode = true;
+    }
+    else {
+      this.mode = false;
+    }
+  }
+
+  editAccount() {
+
+  }
+
   deleteAccount(account: backend.socialAccount) {
     console.log(account);
     this.firebase.deleteSocialAccount(this.type, account);
+    this.mode = false;
   }
 
   addSMAccount() {
     this.firebase.addSocialAccount(this.type, new backend.socialAccount(this.id,this.username,this.url));
     this.adding = false;
+    this.mode = false;
     this.id = "";
     this.username = "";
     this.url = "";
