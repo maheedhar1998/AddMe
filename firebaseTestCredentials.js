15313dc8d9ca5c755d817d571d9e1681ff6ed387
pkellz <patrickscott2013@gmail.com>
2020-04-27 23:19:00 -0400

    Remove firebaseTestCredentials from .gitignore

---
+++ firebaseTestCredentials.js
@@ -0,0 +1,11 @@
+module.exports =  {
+    validUser: {
+        email: 'Aa1234@gmail.com',
+        password: 'Aa1234'
+    },
+    invalidUser: {
+        email: 'comg69@topmail.net',
+        password: 'pswd'
+    },
+    genUid: "3Kmwe883zFQtUrpVcBYyKM4V9Uj1"
+}
