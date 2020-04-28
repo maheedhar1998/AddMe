fb9444812d04fe907ab30850d6d8e919509d4975
maheedhar1998 <maheedhar1998@hotmail.com>
2020-04-27 23:57:42 -0400

    Update git ignore

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyBVy4hzthYjRKiPg5t-bMtB_gOT5oouQkQ",
+    authDomain: "addme-cd3be.firebaseapp.com",
+    databaseURL: "https://addme-cd3be.firebaseio.com",
+    projectId: "addme-cd3be",
+    storageBucket: "addme-cd3be.appspot.com",
+    messagingSenderId: "368953745648",
+    appId: "1:368953745648:web:51fb2a333a3f8046c32235",
+    measurementId: "G-WRWLV6WJ2S"
+  };
